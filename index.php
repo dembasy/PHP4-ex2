a62868d67944253ef4d1974a3ce12d86ed67db9f
dembasy <dembasy1227@gmail.com>
2018-07-30 16:22:12 +0200

    done

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// definition de la fonction chaines qui retournera la chaine de caractere
+  function chaine(){
+    return 'chaine';
+  }
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 1 </title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // affichage de la fonction chaine
+      echo chaine();
+       ?>
+    </p>
+  </body>
+</html>
